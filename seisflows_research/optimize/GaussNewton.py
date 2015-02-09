a7b1d102bc98fc03de2d0b698faa338e551fd619
rmodrak <rmodrak@princeton.edu>
2015-02-09 13:09:31 -0500

    updated GaussNewton modules

--- seisflows_research/optimize/GaussNewton.py
+++ seisflows_research/optimize/GaussNewton.py
@@ -6,18 +6,20 @@ from seisflows.tools.code import loadtxt, savetxt
 from seisflows.tools.config import loadclass, ParameterObj
 from seisflows.optimize import lib
 
-PAR = ParameterObj('parameters')
-PATH = ParameterObj('paths')
+PAR = ParameterObj('SeisflowsParameters')
+PATH = ParameterObj('SeisflowsPaths')
 
 
-class GaussNewton(loadclass('optimize', 'default')):
-    """ Implements truncated Gauss-Newton algorithm
+class GaussNewton(loadclass('optimize', 'base')):
+    """ Implements truncated Newton algorithm
     """
 
-    def __init__(cls):
-        super(GaussNewton, cls).__init__()
+    def check(cls):
+        """ Checks parameters and paths
+        """
+        super(GaussNewton, cls).check()
 
-        if PAR.SCHEME != 'GaussNewton':
+        if 'SCHEME' not in  PAR:
             setattr(PAR, 'SCHEME', 'GaussNewton')
 
         if 'LCGMAX' not in PAR:
@@ -26,40 +28,48 @@ class GaussNewton(loadclass('optimize', 'default')):
         if 'LCGTHRESH' not in PAR:
             setattr(PAR, 'LCGTHRESH', np.inf)
 
+        if 'LCGPRECOND' not in PAR:
+            setattr(PAR, 'LCGPRECOND', 0)
+
+
     def setup(cls):
-        """ description goes here
-        """
-        super(Newton, cls).setup()
-        cls.LCG = lib.LCG(cls.path, PAR.LCGTHRESH, PAR.LCGMAX)
+        super(GaussNewton, cls).setup()
+        cls.LCG = lib.LCG(cls.path, PAR.LCGTHRESH, PAR.LCGMAX, PAR.LCGPRECOND)
+
 
     def initialize_newton(cls):
-        """ description goes here
+        """ Initialize truncated Newton algorithm
         """
         unix.cd(cls.path)
-        cls.iter += 1
 
         m = loadnpy('m_new')
         p = cls.LCG.initialize()
-        cls.delta = 1.e-3/max(abs(p))
+        cls.delta = 1.e-3 * max(abs(p))**-1
+        tmp = p*cls.delta
         savenpy('m_lcg', m + p*cls.delta)
 
-    def update_newton(cls):
-        """ description goes here
+
+    def iterate_newton(cls):
+        """ Iterate truncated Newton algorithm
         """
         unix.cd(cls.path)
 
-        g0 = loadnpy('g_new')
+        g = loadnpy('g_new')
         dg = loadnpy('g_lcg')
 
         p, isdone = cls.LCG.update(dg/cls.delta)
 
+        s = np.dot(g, p)
+        if s >= 0:
+            print ' Newton failed [not a descent direction]'
+            p, isdone = -g, True
+
         if isdone:
             savenpy('p_new', p)
-            savetxt('s_new', cls.delta*np.dot(g0, p))
-
+            savetxt('s_new', cls.delta*np.dot(g, p))
         else:
             m = loadnpy('m_new')
-            cls.delta = 1.e-3/max(abs(p))
+            cls.delta = 1.e-3 * max(abs(p))**-1
             savenpy('m_lcg', m + p*cls.delta)
 
         return isdone
--- seisflows_research/workflow/FwiGaussNewton.py
+++ seisflows_research/workflow/FwiGaussNewton.py
@@ -6,13 +6,66 @@ from seisflows.tools.array import loadnpy, savenpy
 from seisflows.tools.code import divides, exists
 from seisflows.tools.config import loadclass, ParameterObj
 
-PAR = ParameterObj('parameters')
-PATH = ParameterObj('paths')
+PAR = ParameterObj('SeisflowsParameters')
+PATH = ParameterObj('SeisflowsPaths')
+
+import system
+import solver
+import optimize
+import preprocess
+import postprocess
 
 
-class FwiGaussNewton(loadclass('extensions.workflow', 'FwiNewton')):
-    """ Inversion with Gauss-Newton model updates
+class FwiGaussNewton(loadclass('workflow', 'inversion')):
+    """ Inversion with truncated Gauss Newton model updates
     """
 
-    raise NotImplementedError
+    def check(self):
+        super(FwiGaussNewton, self).check()
+
+        if PAR.OPTIMIZE != 'GaussNewton':
+            raise Exception
+
+        if  PAR.PREPROCESS != 'GaussNewton':
+            raise Exception
+
+        if  PAR.POSTPROCESS != 'GaussNewton':
+            raise Exception
+
+
+    def compute_direction(self):
+        """ Computes search direction
+        """
+        self.evaluate_gradient()
+
+        optimize.initialize_newton()
+        for self.ilcg in range(1, PAR.LCGMAX+1):
+            self.apply_hessian()
+            isdone = optimize.iterate_newton()
+            if isdone:
+                break
+
+
+    def apply_hessian(self):
+        """ Computes the action of the Hessian on a given vector through
+          solver calls
+        """
+        if PAR.VERBOSE:
+            print " LCG iteration", self.ilcg
+
+        self.prepare_model(path=PATH.HESS, suffix='lcg')
+
+        system.run('solver', 'apply_hess',
+                   hosts='all',
+                   path=PATH.HESS)
+
+        postprocess.process_kernels(
+            path=PATH.HESS,
+            tag='gauss_newton_lcg')
+
+        unix.rm(PATH.HESS+'_debug')
+        unix.mv(PATH.HESS, PATH.HESS+'_debug')
+        unix.mkdir(PATH.HESS)
+
+
 
