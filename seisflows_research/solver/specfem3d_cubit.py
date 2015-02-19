4e2c5d355b0a75f5747c9692c6cdcfed9280954c
rmodrak <rmodrak@princeton.edu>
2015-02-19 12:16:43 -0500

    updated solver.specfem3d_cubit

--- seisflows_research/solver/specfem3d_cubit.py
+++ seisflows_research/solver/specfem3d_cubit.py
@@ -11,7 +11,7 @@ from seisflows.seistools.shared import getpar, setpar
 from seisflows.tools import unix
 from seisflows.tools.array import loadnpy, savenpy
 from seisflows.tools.code import exists
-from seisflows.tools.config import loadclass, ParameterObj
+from seisflows.tools.config import loadclass, ParameterError, ParameterObj
 
 PAR = ParameterObj('SeisflowsParameters')
 PATH = ParameterObj('SeisflowsPaths')
@@ -28,6 +28,12 @@ class specfem3d_cubit(loadclass('solver', 'specfem3d')):
         """ Checks parameters and paths
         """
         super(specfem3d_cubit, self).check()
+
+        if PAR.FORMAT != 'ascii_specfem3d':
+            raise Exception ('Currently, CUBIT models must be used in combination with ASCII data ouput format.')
+
+        if PAR.WORKFLOW == 'inversion':
+            raise Exception ('The machinery in SPECFEM3D for iteratively updating CUBIT models is in disrepair. As a result, it is only possible to run migration, not inversions.')
 
 
     def generate_data(self, **model_kwargs):
@@ -48,7 +54,13 @@ class specfem3d_cubit(loadclass('solver', 'specfem3d')):
         assert(model_name)
         assert(exists(model_path))
 
+        print 'DEBUGGING INFO'
+        print 'model_name:', model_name
+        print 'model_path:', model_path
+        print ''
+
         self.initialize_solver_directories()
+        unix.touch('OUTPUT_FILES/values_from_mesh.txt')
         unix.cd(self.getpath)
         unix.cp(glob(model_path +'/'+ '*'), self.getpath +'/'+ 'OUTPUT_FILES')
         self.export_model(PATH.OUTPUT +'/'+ model_name)
@@ -72,6 +84,19 @@ class specfem3d_cubit(loadclass('solver', 'specfem3d')):
         unix.ln('traces/adj', 'SEM')
         self.mpirun('bin/xspecfem3D')
 
+
+    def load(self):
+        raise Exception('Not implemented for CUBIT models.')
+
+
+    def save(self):
+        raise Exception('Not implemented for CUBIT models.')
+
+
+    def import_model(self, path):
+        src = join(path, 'model')
+        dst = self.getpath +'/'+ 'OUTPUT_FILES'
+        unix.cp(src, dst)
 
     @property
     def data_wildcard(self):
