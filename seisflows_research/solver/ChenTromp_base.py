0e764baae20fdab843100cfb7ce4b024a5f5fc05
rmodrak <rmodrak@princeton.edu>
2015-03-24 13:07:38 -0400

    updated ChenTromp solver interfaces

--- seisflows_research/solver/ChenTromp_base.py
+++ seisflows_research/solver/ChenTromp_base.py
@@ -4,83 +4,52 @@ PAR = ParameterObj('SeisflowsParameters')
 PATH = ParameterObj('SeisflowsPaths')
 
 
-class ChenTromp_tti(loadclass('extensions.solver', 'specfem3d_legacy')):
-    # data channels
-    channels = []
-    channels += ['x']
+class ChenTromp_base(loadclass('solver', 'ChenTromp_base')):
 
     # model parameters expected by solver
-    model_parameters = []
-    model_parameters += ['rho']
-    model_parameters += ['A']
-    model_parameters += ['C']
-    model_parameters += ['N']
-    model_parameters += ['L']
-    model_parameters += ['F']
-    model_parameters += ['Jc']
-    model_parameters += ['Js']
-    model_parameters += ['Kc']
-    model_parameters += ['Ks']
-    model_parameters += ['Mc']
-    model_parameters += ['Ms']
-    model_parameters += ['Gc']
-    model_parameters += ['Gs']
-    model_parameters += ['Bc']
-    model_parameters += ['Bs']
-    model_parameters += ['Hc']
-    model_parameters += ['Hs']
-    model_parameters += ['Dc']
-    model_parameters += ['Ds']
-    model_parameters += ['Ec']
-    model_parameters += ['Es']
+    solver_parameters = []
+    solver_parameters += ['rho']
+    solver_parameters += ['A']
+    solver_parameters += ['C']
+    solver_parameters += ['N']
+    solver_parameters += ['L']
+    solver_parameters += ['F']
+    solver_parameters += ['Jc']
+    solver_parameters += ['Js']
+    solver_parameters += ['Kc']
+    solver_parameters += ['Ks']
+    solver_parameters += ['Mc']
+    solver_parameters += ['Ms']
+    solver_parameters += ['Gc']
+    solver_parameters += ['Gs']
+    solver_parameters += ['Bc']
+    solver_parameters += ['Bs']
+    solver_parameters += ['Hc']
+    solver_parameters += ['Hs']
+    solver_parameters += ['Dc']
+    solver_parameters += ['Ds']
+    solver_parameters += ['Ec']
+    solver_parameters += ['Es']
 
-    # model parameters included in inversion
-    inversion_parameters = []
-    inversion_parameters += ['A']
-    inversion_parameters += ['C']
-    inversion_parameters += ['L']
-    inversion_parameters += ['N']
-    inversion_parameters += ['F']
-    inversion_parameters += ['Jc']
-    inversion_parameters += ['Js']
-    inversion_parameters += ['Kc']
-    inversion_parameters += ['Ks']
-    inversion_parameters += ['Mc']
-    inversion_parameters += ['Ms']
-    inversion_parameters += ['Gc']
-    inversion_parameters += ['Gs']
-    inversion_parameters += ['Bc']
-    inversion_parameters += ['Bs']
-    inversion_parameters += ['Hc']
-    inversion_parameters += ['Hs']
-    inversion_parameters += ['Dc']
-    inversion_parameters += ['Ds']
-    inversion_parameters += ['Ec']
-    inversion_parameters += ['Es']
 
-    kernel_map = {
-        'rho': 'rho_kernel',
-        'A': 'A_kernel',
-        'C': 'C_kernel',
-        'N': 'N_kernel',
-        'L': 'L_kernel',
-        'F': 'F_kernel',
-        'Jc': 'Jc_kernel',
-        'Js': 'Js_kernel',
-        'Kc': 'Kc_kernel',
-        'Ks': 'Ks_kernel',
-        'Mc': 'Mc_kernel',
-        'Ms': 'Ms_kernel',
-        'Gc': 'Gc_kernel',
-        'Gs': 'Gs_kernel',
-        'Bc': 'Bc_kernel',
-        'Bs': 'Bs_kernel',
-        'Hc': 'Hc_kernel',
-        'Hs': 'Hs_kernel',
-        'Dc': 'Dc_kernel',
-        'Ds': 'Ds_kernel',
-        'Ec': 'Ec_kernel',
-        'Es': 'Es_kernel'}
+    def save(self, path, model, prefix='', suffix=''):
+        unix.mkdir(path)
+
+        for iproc in range(PAR.NPROC):
+            for key in solver.parameters:
+                if key in self.parameters:
+                    savebin(model[key][iproc], path, iproc, prefix+key+suffix)
+                else:
+                    src = PATH.OUTPUT +'/'+ 'model_init'
+                    dst = path
+                    copybin(src, dst, iproc, prefix+key+suffix)
+
+            if 'rho' in self.parameters:
+                savebin(model['rho'][iproc], path, iproc, prefix+'rho'+suffix)
+            else:
+                src = PATH.OUTPUT +'/'+ 'model_init'
+                dst = path
+                copybin(src, dst, iproc, 'rho')
 
 
     def export_kernels(self, path):
