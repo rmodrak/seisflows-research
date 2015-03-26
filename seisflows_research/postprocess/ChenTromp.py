7bac34243d2fb94ef61c15c49bdb0fd9aaa2d08f
rmodrak <rmodrak@princeton.edu>
2015-03-26 14:00:27 -0400

    added postprocess.ChenTromp, postprocess.Thomsen

--- seisflows_research/postprocess/ChenTromp.py
+++ seisflows_research/postprocess/ChenTromp.py
@@ -1,16 +1,51 @@
 
-raise NotImplementedError
+import numpy as np
 
-# compute azimuth
-try:
-    parts = solver.load(path +'/'+ 'kernels/sum', type='kernel')
-    for iproc in range(PAR.NPROC):
-        y = parts['Gs'][iproc]
-        x = - parts['Gc'][iproc]
-        t = 0.5*np.arctan2(y, x)
-        dirname = path +'/'+ tag
-        filename = 'proc%06d_%s.bin' % (iproc, 'azimuth')
-        savebin(t, dirname +'/'+ filename)
-except:
-    pass
+from seisflows.tools import unix
+from seisflows.tools.array import loadnpy, savenpy
+from seisflows.tools.code import exists
+from seisflows.tools.config import loadclass, ParameterObj, ParameterError
+
+PAR = ParameterObj('SeisflowsParameters')
+PATH = ParameterObj('SeisflowsPaths')
+
+import system
+import solver
+
+
+class ChenTromp(loadclass('postprocess', 'base')):
+    """ Postprocessing class
+    """
+
+    def write_gradient(self, path):
+        """ Writes gradient of objective function
+        """
+        # check parameters
+        if 'OPTIMIZE' not in PATH:
+            raise ParameterError(PATH, 'OPTIMIZE')
+
+        # check input arguments
+        if not exists(path):
+            raise Exception()
+
+        self.combine_kernels(path)
+        self.process_kernels(path)
+
+        g = solver.merge(solver.load(
+                path +'/'+ 'kernels/sum',
+                suffix='_kernel',
+                verbose=True))
+
+        # apply scaling
+        if float(PAR.SCALE) == 1.:
+            pass
+        elif not PAR.SCALE:
+            pass
+        else:
+            g *= PAR.SCALE
+
+        # write gradient
+        solver.save(PATH.GRAD +'/'+ 'gradient', solver.split(g), suffix='_kernel')
+        savenpy(PATH.OPTIMIZE +'/'+ 'g_new', g)
+
 
--- seisflows_research/postprocess/Thomsen.py
+++ seisflows_research/postprocess/Thomsen.py
@@ -1,9 +1,10 @@
+
 import numpy as np
 
 from seisflows.tools import unix
 from seisflows.tools.array import loadnpy, savenpy
 from seisflows.tools.code import exists
-from seisflows.tools.config import ParameterObj
+from seisflows.tools.config import loadclass, ParameterObj, ParameterError
 
 PAR = ParameterObj('SeisflowsParameters')
 PATH = ParameterObj('SeisflowsPaths')
@@ -12,96 +13,39 @@ import system
 import solver
 
 
-class Thomsen(object):
+class Thomsen(loadclass('postprocess', 'base')):
     """ Postprocessing class
-
-      Combines contributions from individual sources to obtain the gradient
-      direction, and performs scaling, clipping, smoothing, and preconditioning
-      operations on gradient in accordance with parameter settings.
     """
 
-    def check(self):
-        """ Checks parameters, paths, and dependencies
+    def write_gradient(self, path):
+        """ Writes gradient of objective function
         """
-        # check postprocessing settings
-        if 'SCALE' not in PAR:
-            setattr(PAR, 'SCALE', False)
+        # check parameters
+        if 'OPTIMIZE' not in PATH:
+            raise ParameterError(PATH, 'OPTIMIZE')
 
-        if 'CLIP' not in PAR:
-            setattr(PAR, 'CLIP', 0.)
+        # check input arguments
+        if not exists(path):
+            raise Exception()
 
-        if 'SMOOTH' not in PAR:
-            setattr(PAR, 'SMOOTH', 0.)
+        self.combine_kernels(path)
+        self.process_kernels(path)
 
-        if 'PRECOND' not in PATH:
-            setattr(PATH, 'PRECOND', None)
+        g = solver.merge(solver.load(
+                path +'/'+ 'kernels/sum',
+                suffix='_kernel',
+                verbose=True))
 
+        # apply scaling
+        if float(PAR.SCALE) == 1.:
+            pass
+        elif not PAR.SCALE:
+            pass
+        else:
+            g *= PAR.SCALE
 
-    def setup(self):
-        # nothing to do
-        pass
+        # write gradient
+        solver.save(PATH.GRAD +'/'+ 'gradient', solver.split(g), suffix='_kernel')
+        savenpy(PATH.OPTIMIZE +'/'+ 'g_new', g)
 
-
-    def merge_kernels(self, tag, path):
-        """ Converts kernel dictionary to gradient vector
-        """
-        unix.cd(path + '/' + 'kernels')
-        g = solver.merge(solver.load('sum', type='kernel', verbose=True))
-        return g
-        #unix.cd(path + '/' + 'kernels')
-        #kernels = solver.load('sum', type='kernel', verbose=True)
-        #weights = solver.load('../model', type='model')
-
-        #g = np.array([])
-        #for key in solver.inversion_parameters:
-        #    if key in ['rho','vp','vs']:
-        #        print ' weighting', key
-        #        for iproc in range(PAR.NPROC):
-        #            g = np.append(g, kernels[key][iproc] * weights[key][iproc])
-        #    else:
-        #        for iproc in range(PAR.NPROC):
-        #            g = np.append(g, kernels[key][iproc])
-        #print ''
-        #return g
-
-
-    def process_kernels(self, tag='grad', path=None):
-        """ Computes gradient and performs scaling, smoothing, and 
-          preconditioning operations
-        """
-        assert (exists(path))
-
-        # combine kernels
-        system.run('solver', 'combine',
-                   hosts='head',
-                   path=path + '/' + 'kernels')
-
-        g = self.merge_kernels(tag, path)
-
-        # apply ad hoc scaling
-        if float(PAR.SCALE) != 1.: g *= PAR.SCALE
-        solver.save(path + '/' + tag, solver.split(g))
-
-        # apply clipping
-        if PAR.CLIP > 0.:
-            raise NotImplementedError
-
-        # apply smoothing
-        if PAR.SMOOTH > 0.:
-            system.run('solver', 'smooth',
-                       hosts='head',
-                       path=path,
-                       span=PAR.SMOOTH)
-
-        # apply preconditioner
-        if PATH.PRECOND:
-            unix.cd(path)
-            g /= solver.merge(solver.load(PATH.PRECOND))
-            unix.mv(tag, tag + '_noprecond')
-            solver.save(tag, solver.split(g))
-
-        if 'OPTIMIZE' in PATH:
-            if tag == 'grad':
-                g = solver.merge(solver.load(path +'/'+ tag, type='model', verbose=True))
-                savenpy(PATH.OPTIMIZE +'/'+ 'g_new', g)
 
