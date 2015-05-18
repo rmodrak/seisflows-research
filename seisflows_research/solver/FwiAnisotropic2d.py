9fa92899eca6d5e12ef78ce4faae7f02b9fb8178
rmodrak <rmodrak@princeton.edu>
2015-05-17 22:57:27 -0400

    updated solver.FwiAnisotropic2d

--- seisflows_research/solver/FwiAnisotropic2d.py
+++ seisflows_research/solver/FwiAnisotropic2d.py
@@ -18,20 +18,20 @@ class FwiAnisotropic2d(loadclass('solver', 'FwiElastic'), loadclass('solver', 's
     """ Adds elastic inversion machinery
     """
     model_parameters = []
-    model_parameters += ['C11']
-    model_parameters += ['C13']
-    model_parameters += ['C15']
-    model_parameters += ['C33']
-    model_parameters += ['C35']
-    model_parameters += ['C55']
-    model_parameters += ['C12']
-    model_parameters += ['C23']
-    model_parameters += ['C25']
+    model_parameters += ['c11']
+    model_parameters += ['c13']
+    model_parameters += ['c15']
+    model_parameters += ['c33']
+    model_parameters += ['c35']
+    model_parameters += ['c55']
+    #model_parameters += ['c12']
+    #model_parameters += ['c23']
+    #model_parameters += ['c25']
 
 
     if PAR.MATERIALS == 'ChenTromp':
-        from seisflows.seistools.maps import ChenTromp2d_forward as map_forward
-        from seisflows.seistools.maps import ChenTromp2d_forward as map_inverse
+        from seisflows.seistools.maps import voigt_chentromp_2d as map_forward
+        from seisflows.seistools.maps import chentromp_voigt_2d as map_inverse
         kernel_parameters = []
         kernel_parameters += ['A']
         kernel_parameters += ['C']
@@ -41,23 +41,20 @@ class FwiAnisotropic2d(loadclass('solver', 'FwiElastic'), loadclass('solver', 's
 
 
     elif PAR.MATERIALS == 'Voigt2d':
-        from seisflows.seistools.maps import Voigt2d_forward as map_forward
-        from seisflows.seistools.maps import Voigt2d_inverse as map_inverse
+        from seisflows.seistools.maps import voigt_voigt_2d as map_forward
+        from seisflows.seistools.maps import voigt_voigt_2d as map_inverse
         kernel_parameters = []
-        kernel_parameters += ['C11']
-        kernel_parameters += ['C13']
-        kernel_parameters += ['C15']
-        kernel_parameters += ['C33']
-        kernel_parameters += ['C35']
-        kernel_parameters += ['C55']
-        kernel_parameters += ['C12']
-        kernel_parameters += ['C23']
-        kernel_parameters += ['C25']
+        kernel_parameters += ['c11']
+        kernel_parameters += ['c13']
+        kernel_parameters += ['c15']
+        kernel_parameters += ['c33']
+        kernel_parameters += ['c35']
+        kernel_parameters += ['c55']
 
 
     elif PAR.MATERIALS == 'Thomsen2d':
-        from seisflows.seistools.maps import Thomsen2d_forward as map_forward
-        from seisflows.seistools.maps import Thomsen2d_inverse as map_inverse
+        from seisflows.seistools.maps import voigt_thomsen_2d as map_forward
+        from seisflows.seistools.maps import thomsen_voigt_2d as map_inverse
         kernel_parameters = []
         kernel_parameters += ['vp']
         kernel_parameters += ['vs']
@@ -65,6 +62,9 @@ class FwiAnisotropic2d(loadclass('solver', 'FwiElastic'), loadclass('solver', 's
         kernel_parameters += ['delta']
         kernel_parameters += ['gamma']
         kernel_parameters += ['theta']
+
+    else:
+        raise ParameterError(PAR, 'Thomsen2d')
 
 
 
