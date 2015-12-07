5b4da15b9cfd1a5cab96f0f3a8f7664044621505
rmodrak <rmodrak@princeton.edu>
2015-12-07 15:50:49 -0500

    fixed regression in solver.lambda_mu_2d

--- seisflows_research/solver/lambda_mu_2d.py
+++ seisflows_research/solver/lambda_mu_2d.py
@@ -50,8 +50,8 @@ class lambda_mu_2d(loadclass('solver', 'elastic2d')):
         model = getstruct(loadbypar(self.getpath+'/'+'DATA/', model_parameters, iproc))
         kernels = getstruct(loadbypar(self.getpath+'/'+'OUTPUT_FILES/', kernel_parameters, iproc, suffix='_kernel'))
 
-        unix.mkdir(join(path, basename(self.path)))
-        self.save(join(path, basename(self.path)), map(model, kernels), suffix='_kernel')
+        unix.mkdir(join(path, basename(self.getpath)))
+        self.save(join(path, basename(self.getpath)), map(model, kernels), suffix='_kernel')
 
         
 
