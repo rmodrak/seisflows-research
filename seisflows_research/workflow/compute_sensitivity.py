932223c7823a10aabb22b51296b4a6fb6ec16c2f
rmodrak <rmodrak@princeton.edu>
2015-12-08 18:33:00 -0500

    prototyped workflow.compute_sensitivity

--- seisflows_research/workflow/compute_sensitivity.py
+++ seisflows_research/workflow/compute_sensitivity.py
@@ -1,49 +1,98 @@
 
+from glob import glob
+from os.path import join
+
+import numpy as np
+
+from seisflows.tools import unix
+from seisflows.tools.code import exists
 from seisflows.tools.config import SeisflowsParameters, SeisflowsPaths, \
-    ParameterError, loadclass
+    ParameterError
+
+from seisflows.seistools import adjoint
 
 PAR = SeisflowsParameters()
 PATH = SeisflowsPaths()
 
 import system
 import solver
+import preprocess
+import postprocess
 
 
 class compute_sensitivity(object):
-    """ Tests solver by running forward simulation
-    """
 
     def check(self):
         """ Checks parameters and paths
         """
-
         # check paths
         if 'GLOBAL' not in PATH:
-            raise Exception
+            raise ParameterError(PATH, 'GLOBAL')
 
         if 'LOCAL' not in PATH:
             setattr(PATH, 'LOCAL', None)
 
-        if 'MODEL_INIT' not in PATH:
-            raise Exception
-
-        if 'MODEL_PERT' not in PATH:
-            raise Exception
-
         if 'OUTPUT' not in PATH:
-            raise Exception
+            raise ParameterError(PATH, 'OUTPUT')
+
+        # check input
+        if 'DATA' not in PATH:
+            setattr(PATH, 'DATA', None)
+
+        if 'MODEL' not in PATH:
+            raise ParameterError(PATH, 'MODEL')
+
+        if 'PERTURB' not in PATH:
+            raise ParameterError(PATH, 'PERTURB')
+
+        if 'MODEL_INIT' not in PATH:
+            setattr(PATH, 'MODEL_INIT', PATH.MODEL)
+
+        if 'MODEL_TRUE' not in PATH:
+            setattr(PATH, 'MODEL_TRUE', PATH.MODEL)
 
 
     def main(self):
-        """ Generates seismic data
+        """ Computes point spread from a peturbation dm with respect to a
+          refrence model m
         """
+        self.setup()
 
-        print 'Running solver...'
+        m = solver.merge(solver.load(PATH.MODEL))
+        dm = solver.merge(solver.load(PATH.PERTURB))
 
-        system.run('solver', 'generate_data',
+        self.evaluate_gradient(m+dm, join(PATH.GLOBAL, 'eval1'))
+        self.evaluate_gradient(m-dm, join(PATH.GLOBAL, 'eval2'))
+
+
+    def setup(self):
+        path = PATH.GLOBAL
+
+        # prepare directory structure
+        unix.rm(path)
+        unix.mkdir(path)
+
+        # set up workflow machinery
+        preprocess.setup()
+        postprocess.setup()
+
+        system.run('solver', 'setup',
+                   hosts='all')
+
+
+    def evaluate_gradient(self, model, path):
+        """ Performs forward simulation to evaluate objective function
+        """
+        solver.save(join(path, 'model'), solver.split(model))
+
+        system.run('solver', 'eval_func',
                    hosts='all',
-                   model_path=PATH.MODEL,
-                   model_type='gll',
-                   model_name='model')
+                   path=path)
 
-        print "Finished"
+        system.run('solver', 'eval_grad',
+                   hosts='all',
+                   path=path)
+
+        postprocess.write_gradient(
+            path=path)
+
